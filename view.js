9b0bc042501a7af34a32007af7da07ff94bdf465
Timothy Abiola <99691683+timothyAbiola@users.noreply.github.com>
2023-08-24 22:12:51 +0100

    my first file upload

---
+++ view.js
@@ -0,0 +1,21 @@
+(function display() {
+  let views = document.getElementById("view");
+  let mainDiv = document.getElementById("mainDiv");
+  const stock = document.getElementById("stock")
+  const product = JSON.parse(localStorage.getItem("product"));
+    console.log("new product", product)
+    views.innerHTML = product[0].category;
+    mainDiv.innerHTML = product[0].category;
+  product.forEach(item => {
+    let desc = item.description
+    if (desc.length >= 25) {
+      desc = item.description.slice(0, 24);
+      stock.innerHTML += `<div class="perStock"> 
+        <img src="${item.image}" class="stockImg"> 
+        <p class="desc">${desc}...</p>
+        <p class="s-price">₦${item.price}</p>
+        <p class="s-through">₦${item.ip}</p>
+      </div>`;
+    }
+    });
+}())
